15bc37f321593ed6e1232cabcbb637c8bbcf80f5
jmilanez <josiane.milanez@gmail.com>
2019-02-05 17:52:24 +0100

    Adding api key for now

--- src/main/kotlin/com/jomilanez/apigateway/ApiSecurityConfiguration.kt
+++ src/main/kotlin/com/jomilanez/apigateway/ApiSecurityConfiguration.kt
@@ -1,40 +1,35 @@
 package com.jomilanez.apigateway
 
 import org.springframework.context.annotation.Bean
+import org.springframework.security.authentication.ReactiveAuthenticationManager
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
-import org.springframework.security.core.userdetails.MapReactiveUserDetailsService
-import org.springframework.security.core.userdetails.User
 import org.springframework.security.web.server.SecurityWebFilterChain
 
-
+@EnableReactiveMethodSecurity
 @EnableWebFluxSecurity
-class ApiSecurityConfiguration {
-
-    @Bean
-    fun userDetailsService(): MapReactiveUserDetailsService {
-        val user = User.withDefaultPasswordEncoder()
-            .username("admin")
-            .password("password")
-            .roles("ADMIN")
-            .build()
-        return MapReactiveUserDetailsService(user)
-    }
-
+class ApiSecurityConfiguration(
+    val authenticationManager: ReactiveAuthenticationManager,
+    val securityContextRepository: SecurityContextRepository
+) {
 
     @Bean
     fun securityWebFilterChain(
         http: ServerHttpSecurity
     ): SecurityWebFilterChain {
-        return http.csrf()
-            .disable()
+        return http
+            .csrf().disable()
+            .formLogin().disable()
+            .securityContextRepository(securityContextRepository)
+            .authenticationManager(authenticationManager)
             .authorizeExchange()
             .pathMatchers(
                 "/v2/api-docs/**",
                 "/swagger-resources/**",
                 "/swagger-ui.html**",
                 "/webjars/**",
-                "/actuator/*"
+                "/actuator/**"
             ).permitAll()
             .anyExchange().authenticated()
             .and().build()
