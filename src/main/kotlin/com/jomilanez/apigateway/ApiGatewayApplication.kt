0067773a9b4206262723278c0b9965ee3e463e3b
jmilanez <josiane.milanez@gmail.com>
2019-04-12 07:59:02 +0200

    Adding swagger

--- src/main/kotlin/com/jomilanez/apigateway/ApiGatewayApplication.kt
+++ src/main/kotlin/com/jomilanez/apigateway/ApiGatewayApplication.kt
@@ -2,8 +2,10 @@ package com.jomilanez.apigateway
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux
 
 @SpringBootApplication
+@EnableSwagger2WebFlux
 class ApiGatewayApplication
 
 fun main(args: Array<String>) {
